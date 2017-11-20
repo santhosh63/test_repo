c652a7a9d6d87f2cafa6d08e49fe198880584bb3
santhosh <santhohnalla1993@gmail.com>
2017-11-20 10:40:08 -0500

    modifying jenkinsfile

--- simple-maven-project-with-tests-testng/jenkinsfile.groovy
+++ simple-maven-project-with-tests-testng/jenkinsfile.groovy
@@ -1,9 +1,9 @@
 node {
    def mvnHome
-    checkout scm
     // sh ("git checkout master && git pull origin master")
 
     stage ('start') {
+       checkout scm
         // Get the maven tool.
         // ** NOTE: This 'M3' maven tool must be configured
         // **       in the global configuration
@@ -11,9 +11,9 @@ node {
     }
     stage ('Build') {
         if ('isUnix()') {
-            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install"
         } else {
-            bat "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+            bat "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install"
         }
     }
     stage('Unit Testing') {
